cc4e0bc6f3c84d8498186f94ab71527e3163f6b5
Gabr1el94 <bielsystem64@gmail.com>
2022-06-23 22:37:36 -0300

    simple page clock with date today in js

---
+++ page-clock/js/script.js
@@ -0,0 +1,29 @@
+const dateToday = document.getElementById('dateToday');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time() {
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    if (hr < 10) {
+        hr = '0'+hr;
+    }
+
+    if (min < 10) {
+        min = '0'+min;
+    }
+
+    if (sec < 10) {
+        sec = '0'+sec;
+    }
+
+    hours.textContent = hr;
+    minutes.textContent = min;
+    seconds.textContent = sec;
+
+    dateToday.textContent = date.getMonth()+'/'+date.getDate()+'/'+date.getFullYear()
+});
